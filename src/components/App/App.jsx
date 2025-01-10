7fdd5110cec6b9d7a554c5ff4d031c6c2e9a5750
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-10 22:30:25 +0200

    add folders Loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<h1>Marina</h1>} />
+        <Route path="/about" element={<h1>All</h1>} />
+      </Routes>
+    </div>
+  );
 }
