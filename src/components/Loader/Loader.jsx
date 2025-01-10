9445279260f717e3f6a2529d0b3c372c82644d0c
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-10 22:46:19 +0200

    install react-loader-spinner

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import { Hearts } from "react-loader-spinner";
+import css from "./Loader.module.css";
+
+export default function Loader() {
+  return (
+    <div className={css.loader}>
+      <Hearts
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="hearts-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+}
