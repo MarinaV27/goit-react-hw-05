86615fce6a5dada1c2dc5eedb9dda21f55a3ed4f
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-10 23:22:26 +0200

    add css to Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,21 @@
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
-import css from "./Navigation.module.css"
+import css from "./Navigation.module.css";
 
+const linkClass = ({ isActiv }) => {
+  return clsx(css.link, isActiv && css.active);
+};
 
 export default function Navigation() {
   return (
     <header className={css.header}>
       <nav className={css.nav}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/">Movies</NavLink>
+        <NavLink to="/" className={linkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/muvies" className={linkClass}>
+          Movies
+        </NavLink>
       </nav>
     </header>
   );
