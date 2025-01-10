c6b2fdb261f47bd00151332f55926fe89df64ec2
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-10 23:46:29 +0200

    add isActive in Navigation css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 
 export default function App() {
   return (
     <div>
+      <Navigation />
       <Routes>
         <Route path="/" element={<h1>Marina</h1>} />
-        <Route path="/about" element={<h1>All</h1>} />
+        <Route path="/muvies" element={<h1>All</h1>} />
       </Routes>
     </div>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import css from "./Navigation.module.css";
 
-const linkClass = ({ isActiv }) => {
-  return clsx(css.link, isActiv && css.active);
+const buildlinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
 };
 
 export default function Navigation() {
   return (
     <header className={css.header}>
       <nav className={css.nav}>
-        <NavLink to="/" className={linkClass}>
+        <NavLink to="/" className={buildlinkClass}>
           Home
         </NavLink>
-        <NavLink to="/muvies" className={linkClass}>
+        <NavLink to="/muvies" className={buildlinkClass}>
           Movies
         </NavLink>
       </nav>
