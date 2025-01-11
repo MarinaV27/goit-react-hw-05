4258121f8a3c125cb392b809fffdb113f67f4063
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-11 21:26:33 +0200

    Outlet in MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,12 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const MovieDetailsPage = () => {
     return <div>
         <nav>
-            <NavLink to="actors">Actors Info</NavLink>
+            <NavLink to="cast">Actors Info</NavLink>
             <NavLink to="reviews">Movie Reviews</NavLink>
         </nav>
-        
+        <Outlet/>
     </div>
 }
 export default MovieDetailsPage;
