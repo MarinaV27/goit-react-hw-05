d27a6d4d5232722b4032918d10102f31d3a2a2ac
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-11 20:13:14 +0200

    page 404 in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import HomePage from "../../pages/HomePage/HomePage";
+import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+
 
 export default function App() {
   return (
     <div>
       <Navigation />
       <Routes>
-        <Route path="/" element={<h1>HomePage</h1>} />
-        <Route path="/muvies" element={<h1>MoviesPage</h1>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/muvies" element={<MoviesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
