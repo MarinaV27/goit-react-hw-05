34950edd2520fb5e174c70251b9ae60e87ccf8f8
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-11 22:19:21 +0200

    add api in movies-api

---
+++ src/movies-api.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+// const API_KEY = "8382974d19cb9324dcad053cce7db68a"
+// const API_TOKEN = 
+// "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzgyOTc0ZDE5Y2I5MzI0ZGNhZDA1M2NjZTdkYjY4YSIsIm5iZiI6MTczNjUzMDU1NC4wMTIsInN1YiI6IjY3ODE1YTdhMzg4MWM3OTQxOWJiNzFmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PG1qiTWSxGFaSoB5zKGJsjItk3x1zDgX2hdyTJHH_NU"
+
+axios.defaults.baseURL = "https://api.themoviedb.org"
