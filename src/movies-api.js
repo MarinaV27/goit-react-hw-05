d8bc7307d5f88ff486522a3a3825be6149c38639
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-11 23:25:58 +0200

    add MoviesList

--- src/movies-api.js
+++ src/movies-api.js
@@ -3,4 +3,21 @@ import axios from "axios"
 // const API_TOKEN = 
 // "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzgyOTc0ZDE5Y2I5MzI0ZGNhZDA1M2NjZTdkYjY4YSIsIm5iZiI6MTczNjUzMDU1NC4wMTIsInN1YiI6IjY3ODE1YTdhMzg4MWM3OTQxOWJiNzFmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PG1qiTWSxGFaSoB5zKGJsjItk3x1zDgX2hdyTJHH_NU"
 
-axios.defaults.baseURL = "https://api.themoviedb.org"
+axios.defaults.baseURL = "https://api.themoviedb.org/3"
+const options = {
+  params: {
+    include_adult: false,
+    language: 'en-US',
+    page: 1,
+    },
+    
+     headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzgyOTc0ZDE5Y2I5MzI0ZGNhZDA1M2NjZTdkYjY4YSIsIm5iZiI6MTczNjUzMDU1NC4wMTIsInN1YiI6IjY3ODE1YTdhMzg4MWM3OTQxOWJiNzFmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PG1qiTWSxGFaSoB5zKGJsjItk3x1zDgX2hdyTJHH_NU',
+  },
+};
+
+export const fetchMovies = async () => {
+  const response = await axios.get(`/trending/movie/day`, options);
+  return response.data.results;
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
+import { fetchMovies } from "../../movies-api";
+import MovieList from "../../components/MovieList/MovieList";
+
+
 const HomePage = () => {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await fetchMovies();
+            setMovies(data);
+        }
+        getData()
+    }, [])
+
+
     return <div>
-        HomePage
+        
+        {movies.length > 0 && <MovieList movies={movies} />}
+
     </div>
 }
 export default HomePage;
