6843475b36b1d13adb1605f358a1ec261bceaa97
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-12 00:09:38 +0200

    add toString to id in MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,8 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import css from './MovieList.module.css';
 
 export default function MovieList({ movies }) {
-  const location = useLocation();
+  //const location = useLocation();
 
     return (<>
   <h2 className={css.title}>Trending movies </h2>
@@ -10,7 +10,7 @@ export default function MovieList({ movies }) {
       {movies.map(movie => {
           return (
           <li className={css.item} key={movie.id}>
-            <Link to={`/movies/${movie.id}`} state={location}>
+            <Link to={`/movies/${movie.id.toString()}`} >
               <img
                 src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
               />
